a20fd9e6b1165079ed6e2c2089aba1e667dfd135
Ethan_Willingham <568352@gmail.com>
2022-09-01 23:53:07 -0500

    Testing git using terminal***THIS IS ONLY A TEST***

---
+++ Driver.java
@@ -0,0 +1,32 @@
+//Ethan Willingham
+//September 1st, 2022
+//ICS 372
+//TEST_FILE
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Driver {
+
+    public static void helloWorld() {
+        String systemTime = String.valueOf(java.time.Clock.systemDefaultZone().instant());
+            String time = systemTime.substring(11, systemTime.length() - 11);
+        if (time.charAt(0) >= 0 && time.charAt(0) <= 11) {
+            System.out.println("Good Morning, World!");
+        } else if (time.charAt(0) >= 12 && time.charAt(0) <= 17) {
+            System.out.println("Good Afternoon, World!");
+        } else if (time.charAt(0) >= 18 && time.charAt(0) <= 23) {
+            System.out.println("Good Evening, World!");
+        }
+        else {
+            System.out.println("Good Night, World!");
+        }
+        }
+
+    public static void main(String[] args) {
+        helloWorld();
+    }
+}
