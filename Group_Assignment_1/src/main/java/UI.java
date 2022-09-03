55553daa639f19b7c2ca2246ad9403ddb3b13ba4
Ethan_Willingham <568352@gmail.com>
2022-09-02 21:27:23 -0500

    implemented serialize and deserialize methods

--- Group_Assignment_1/src/main/java/UI.java
+++ Group_Assignment_1/src/main/java/UI.java
@@ -1,2 +1,30 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 public class UI {
+    public static void main(String[] args) {
+        serialize();
+        deserialize();
+    }
+
+    private static void serialize() {
+        JSONconversion json = new JSONconversion(
+                "1",
+                "car",
+                "Toyota",
+                "Camry",
+                "1",
+                10000,
+                123456789);
+        Gson gson = new Gson();
+        String jsonStr = gson.toJson(json);
+        //this creates a proper JSON from objects, but has no whitespace or indentation
+        System.out.println(jsonStr);
+    }
+    private static void deserialize() {
+        String jsonStr = "{\"dealership_id\":\"1\",\"vehicle_type\":\"car\",\"vehicle_manufacturer\":\"Toyota\",\"vehicle_model\":\"Camry\",\"vehicle_id\":\"1\",\"price\":10000,\"acquisition_date\":123456789}";
+        Gson gson = new Gson();
+        JSONconversion json = gson.fromJson(jsonStr, JSONconversion.class);
+
+        System.out.println(json);
+    }
 }
