e80f9a1e0c3f5f69fde0d15dc8cf35221ee3c4b1
Dpellowski <danielpellowski@gmail.com>
2022-09-07 20:00:18 -0500

    Fixed

--- Group_Assignment_1/src/main/java/Converters.java
+++ Group_Assignment_1/src/main/java/Converters.java
@@ -1,20 +1,13 @@
 import Models.Inventory;
 import com.google.gson.*;
-import com.google.gson.reflect.TypeToken;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Converter {
-    public static List<Inventory> Converter(FileReader json){
+public class Converters {
+    public List<Inventory> FromJsonToInvArr(FileReader json){
 
-        //Object obj = parser.parse(new FileReader("C:/Users/ahapp/IdeaProjects/ICS-372-Group-Project-1/Group_Assignment_1/src/main/java/Project1_input.json"));
         JsonObject obj = JsonParser.parseReader(json).getAsJsonObject();
 
         JsonArray jCars = obj.get("car_inventory").getAsJsonArray();
@@ -25,12 +18,11 @@ public class Converter {
             Inventory car = new Inventory(
                     c.getAsJsonObject().get("dealership_id").getAsString(),
                     c.getAsJsonObject().get("vehicle_type").getAsString(),
-                    c.getAsJsonObject().get("vehicle_manufacturer").getAsString(),
+                    c.getAsJsonObject().get("vehicle_manufacturer").getAsString(), // <- Breaks on third iteration
                     c.getAsJsonObject().get("vehicle_model").getAsString(),
                     c.getAsJsonObject().get("vehicle_id").getAsString(),
                     c.getAsJsonObject().get("price").getAsInt(),
                     c.getAsJsonObject().get("acquisition_date").getAsLong()
-
             );
 
             cars.add(car);
--- Group_Assignment_1/src/main/java/UI.java
+++ Group_Assignment_1/src/main/java/UI.java
@@ -1,9 +1,18 @@
-import com.google.gson.*;
+import Models.Inventory;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.List;
 
 public class UI {
     public static void main(String[] args) throws FileNotFoundException {
+
+        // Change this to the FilePicker
+        Converters c = new Converters();
+        List<Inventory> al;
+        FileReader test = new FileReader("C:\\Users\\ahapp\\IdeaProjects\\ICS-372-Group-Project-1\\Group_Assignment_1\\src\\main\\java\\Project1_input.json");
+
+        al = c.FromJsonToInvArr(test);
+
     }
 }
