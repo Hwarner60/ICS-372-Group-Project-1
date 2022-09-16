b58462e4feefd70f2f49c93e6a9c4df942e67426
XuezengYang15 <112597516+XuezengYang15@users.noreply.github.com>
2022-09-15 23:27:51 -0500

    Update Location.java

--- Group_Assignment_1/src/main/java/Models/Location.java
+++ Group_Assignment_1/src/main/java/Models/Location.java
@@ -1,4 +1,12 @@
 package Models;
 
 public class Location {
+  
+  private boolean isActivated = true;
+  private String dealer_id = "";
+  
+  
+  public boolean getIsActivatedStatus(){
+        return isActivated;
+    }
 }
