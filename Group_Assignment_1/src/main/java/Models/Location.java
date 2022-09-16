f83baf3afced03153e0067a1d0e6bc68c047492b
XuezengYang15 <112597516+XuezengYang15@users.noreply.github.com>
2022-09-16 14:48:47 -0500

    Update Location.java

--- Group_Assignment_1/src/main/java/Models/Location.java
+++ Group_Assignment_1/src/main/java/Models/Location.java
@@ -4,21 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Location {
-  
-  private boolean isActivated = true;
-  private String dealer_id;
-  List<Inventory> listOfCarsAtLocation = new ArrayList<Inventory>();
-  
-  
-  public boolean getIsActivatedStatus(){
+
+    private boolean isActivated = true;
+    private String dealer_id;
+    List<Inventory> listOfCarsAtLocation = new ArrayList<Inventory>();
+
+    public boolean getIsActivatedStatus(){
         return isActivated;
     }
-  
-  public void setIsActivatedStatus(boolean status) {
+    public void setIsActivatedStatus(boolean status) {
         isActivated = status;
     }
-  
-  public Location(String id, boolean status) {
+
+    public String getDealer_id() {
+        return dealer_id;
+    }
+    public void setDealer_id(String id) {
+        dealer_id = id;
+    }
+
+    public List getListOfCarsAtLocation() {
+        return listOfCarsAtLocation;
+    }
+
+    public void addToListOfCarsAtLocation(Inventory car){
+        listOfCarsAtLocation.add(car);
+    }
+
+    public Location(String id, boolean status) {
         dealer_id = id;
         isActivated = status;
     }
@@ -26,5 +39,4 @@ public class Location {
     public Location() {
 
     }
-
 }
