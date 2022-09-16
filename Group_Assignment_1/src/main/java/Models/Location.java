5029b3314b17c1809cd7f56bc0237b4d4f08d5ef
XuezengYang15 <112597516+XuezengYang15@users.noreply.github.com>
2022-09-16 00:46:51 -0500

    Added List for Cars

--- Group_Assignment_1/src/main/java/Models/Location.java
+++ Group_Assignment_1/src/main/java/Models/Location.java
@@ -1,12 +1,20 @@
 package Models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Location {
   
   private boolean isActivated = true;
-  private String dealer_id = "";
+  private int dealer_id;
+  List<Inventory> listOfCarsAtLocation = new ArrayList<Inventory>();
   
   
   public boolean getIsActivatedStatus(){
         return isActivated;
+    }
+  
+  public void setIsActivatedStatus(boolean status) {
+        isActivated = status;
     }
 }
