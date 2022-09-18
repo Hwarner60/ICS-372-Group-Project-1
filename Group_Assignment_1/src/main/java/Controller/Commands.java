70e4474e3a5beae0d140b4f2ebb72837474ea418
Daniel <danielpellowski@gmail.com>
2022-09-18 09:59:29 -0500

    Changed UI commands to fit MVC format

---
+++ Group_Assignment_1/src/main/java/Controller/Commands.java
@@ -0,0 +1,175 @@
+package Controller;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import Models.Inventory;
+import Models.Location;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+
+public class Commands {
+    JFileChooser fileChooser = new JFileChooser();
+    JButton open = new JButton();
+    List<Location> listOfDealers = new ArrayList<>();
+
+    Converters c = new Converters();
+
+    String outputMessage = "";
+    String outputMessage2 = "";
+
+    Scanner input = new Scanner(System.in);
+
+    public void ReadJSON() throws FileNotFoundException {
+
+        fileChooser.setCurrentDirectory(new java.io.File("C:/Users"));
+        // Titles the text box
+        fileChooser.setDialogTitle("JSON file to Converter");
+        if (fileChooser.showOpenDialog(open) == JFileChooser.APPROVE_OPTION) {
+
+        }
+        // Gets the absolute path of the file
+        String fileAbsolutePath = fileChooser.getSelectedFile().getAbsolutePath();
+
+        outputMessage2 = "The file you chose to read: " + fileAbsolutePath + ".\n";
+
+        FileReader file = new FileReader(fileAbsolutePath);
+
+        List<Inventory> listOfCars;
+
+        listOfCars = c.FromJsonToInvArr(file);
+
+        // if listOfDealers is empty, add the dealer of the first car in listOfCars to
+        // listOfDealers
+        if (listOfDealers.size() == 0 && listOfCars.size() > 0) {
+            listOfDealers.add(new Location(listOfCars.get(0).getDealership_id(), true));
+        }
+        // put cars from json file into dealers
+        // create new dealer if car's dealership_id does not match any existing dealers
+        for (Inventory car : listOfCars) {
+            for (int i = 0; i < listOfDealers.size(); i++) {
+                Location dealer = listOfDealers.get(i);
+                if (car.getDealership_id().equals(dealer.getDealer_id())) {
+                    dealer.addToListOfCarsAtLocation(car);
+                    break;
+                }
+                if (dealer == listOfDealers.get(listOfDealers.size() - 1)) {
+                    listOfDealers.add(new Location(car.getDealership_id(), true));
+                }
+
+            }
+        }
+    }
+
+    public void AddCar() {
+        System.out.println("Enter the dealer id of the dealership: ");
+        String dealership_id = input.nextLine();
+
+        for (int i = 0; i < listOfDealers.size(); i++) {
+
+            // If the vehicle acquisition status is false, dealer can't add cars
+            if (listOfDealers.get(i).getIsActivatedStatus() == true) {
+
+                if (dealership_id.equals(listOfDealers.get(i).getDealer_id())) {
+
+                    System.out.println("Enter the vehicle type? ");
+                    String vehicle_type = input.nextLine();
+
+                    System.out.println("Enter the vehicle manufacturer? ");
+                    String vehicle_manufacturer = input.nextLine();
+
+                    System.out.println("Enter the vehicle model? ");
+                    String vehicle_model = input.nextLine();
+
+                    System.out.println("Enter the vehicle id? ");
+                    String vehicle_id = input.nextLine();
+
+                    System.out.println("Enter the vehicle price? ");
+                    int vehicle_price = input.nextInt();
+
+                    System.out.println("Enter the acquisition date? ");
+                    long acquisition_date = input.nextLong();
+
+                    Inventory car = new Inventory(dealership_id, vehicle_type, vehicle_manufacturer,
+                            vehicle_model, vehicle_id, vehicle_price, acquisition_date);
+
+                    listOfDealers.get(i).addToListOfCarsAtLocation(car);
+                } else {
+                    outputMessage2 = "That dealership does not exist.\n";
+                }
+            } else {
+                outputMessage2 = "That dealership is not receiving cars at the moment.\n";
+            }
+        }
+
+    }
+
+    public void DealerOff() {
+        System.out.println("Enter the dealer id of the dealership: ");
+        String dealerId = input.nextLine();
+
+        for (int i = 0; i < listOfDealers.size(); i++) {
+            if (dealerId.equals(listOfDealers.get(i).getDealer_id())) {
+                listOfDealers.get(i).setIsActivatedStatus(false);
+                // Debug
+                System.out.println(listOfDealers.get(i).getDealer_id());
+                System.out.println(listOfDealers.get(i).getIsActivatedStatus());
+            } else {
+                outputMessage2 = "Dealer ID" + dealerId + " does not exist.\n";
+            }
+        }
+    }
+
+    public void DealerOn() {
+        System.out.println("Enter the dealer id of the dealership: ");
+        String idString = input.nextLine();
+
+        for (int i = 0; i < listOfDealers.size(); i++) {
+            if (idString.equals(listOfDealers.get(i).getDealer_id())) {
+                listOfDealers.get(i).setIsActivatedStatus(true);
+            } else {
+                outputMessage2 = "Dealer ID" + idString + " does not exist.\n";
+            }
+        }
+    }
+
+    public void ExportAll() {
+
+        System.out.println("What is the integer ID for the dealership? ");
+        // Show dealers
+        for (Location dealer : listOfDealers) {
+            System.out.println(dealer.getDealer_id());
+        }
+        String input1 = input.nextLine();
+
+        for (int i = 0; i < listOfDealers.size(); i++) {
+
+            if (input1.equalsIgnoreCase(listOfDealers.get(i).getDealer_id())) {
+                c.convertToJson(listOfDealers.get(i));
+            }
+        }
+    }
+
+    public void Create() {
+        System.out.println("What is the integer ID for the new dealership? ");
+        String idResponse = input.nextLine();
+
+        Location dealer = new Location(idResponse, true);
+
+        listOfDealers.add(dealer);
+    }
+
+    public void ShowDealer() {
+        for (int i = 0; i < listOfDealers.size(); i++) {
+            outputMessage2 += "\nDealership ID:" + listOfDealers.get(i).getDealer_id() +
+                    " Dealership Status: " + listOfDealers.get(i).getIsActivatedStatus() + "\n";
+        }
+        System.out.println("\n");
+
+    }
+
+}
--- Group_Assignment_1/src/main/java/View/MenuPrint.java
+++ Group_Assignment_1/src/main/java/View/MenuPrint.java
@@ -2,19 +2,18 @@ package View;
 
 public class MenuPrint {
 
-    public String getMenuMessage(){
-        String message =
-                """
-                        1)Read JSON file from your C: drive\s
-                        2)Add a car to a dealership\s
-                        3)Turn off the dealership acquisition\s
-                        4)Turn on the dealership acquisition\s
-                        5)Check acquisition status of dealership\s
-                        6)Show list of all current vehicles from a dealership\s
-                        7)Export all vehicles from a dealership into a single JSON file\s
-                        8)Create a dealership\s
-                        0)Exit Program
-                        """;
+    public String getMenuMessage() {
+        String message = """
+                ReadJSON - Read JSON file from your C: drive\s
+                AddCar - Add a car to a dealership\s
+                DealerOff - Turn off the dealership acquisition\s
+                DealerOn - Turn on the dealership acquisition\s
+                CheckStatus - Check acquisition status of dealership\s
+                ShowList - Show list of all current vehicles from a dealership\s
+                ExportAll - Export all vehicles from a dealership into a single JSON file\s
+                Create - Create a dealership\s
+                ShowDealer - Exit Program
+                """;
         return message;
     }
 }
